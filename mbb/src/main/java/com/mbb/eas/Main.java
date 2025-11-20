db87e8a506fd117be0ff7aeadcbb358062f287aa
raihaniooo <raihanaaaz2@gmail.com>
2025-11-20 13:56:30 +0700

    connect to database

--- mbb/src/main/java/com/mbb/eas/Main.java
+++ mbb/src/main/java/com/mbb/eas/Main.java
@@ -1,7 +1,19 @@
 package com.mbb.eas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "jdbc:mysql://localhost:3308/pbo-mbb";
+        String user = "root";
+        String pass = "";
+
+        try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Connected!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
